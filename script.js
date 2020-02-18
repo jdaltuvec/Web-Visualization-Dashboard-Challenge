431a71aa8d5e6b4261f81493418fb8af0258a179
David A <josedavac@gmail.com>
2020-02-17 21:47:31 -0600

    v1 is ready

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(".dropdown-menu").dropdown();
+});
+
+$(".img-fluid").addClass("wow fadeIn z-depth-1-half");
+
+new WOW().init();
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
